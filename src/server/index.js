7021da0f270f47143bfb745a6d8d24e45953654a
AlexanderT <38465150+AlexaTack@users.noreply.github.com>
2020-12-20 23:23:21 +0100

    App in index.js now

--- src/server/index.js
+++ src/server/index.js
@@ -1,4 +1,26 @@
-const app = require('./app');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
+const api = require('./api');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.json({ message: 'Welcome home' }));
+
+app.use('/api/v1', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening (${port})`));
